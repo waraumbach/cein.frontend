bf0681641196109f0e3ad40635d60bcb089bf7b2
waraumbach <waraumbach@gmail.com>
2024-07-30 11:21:34 +0200

    changed hover navbar

--- src/Components/Hero/HeroSection.jsx
+++ src/Components/Hero/HeroSection.jsx
@@ -8,8 +8,7 @@ const HeroSection = () => {
         <div className="ml-2  ">
           <h1 className="text-xl md:text-xl text-gray font-bold mb-4">Treat Your Body like Your Face</h1>
           <p className="text-xl md:text-l text-gray">Creating the hightly Effective Body Care</p><br />
-          <a href="/detailsProduct" className="text-stone px-4 py-2 border border-transparent rounded-md hover:bg-green-300
-           text-gray bg-gray-100 rounded-10px text-lg">get infos</a>
+          <a href="/detailsProduct" className="text-stone px-4 py-2  border-transparent border border-stone-600 rounded hover:bg-gray-300 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0text-lg">get infos</a>
           </div>
         </div>
       </div>
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -15,7 +15,7 @@ function NavBar() {
   return (
     <nav className='bg-stone-400 p-4 bg-cover flex-wrap: wrap;'>
         <div className="flex items-center justify-between">
-            <div className='text-white px-4 py-2 border border-transparent rounded-md hover:bg-green-300 text-2xl font-bold ml-5 cursor-pointer'>
+            <div className='text-lg  ml-5 cursor-pointer py-2 px-4 bg-transparent text-white-600 font-semibold border border-stone-600 rounded hover:bg-gray-300 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0'>
                 cein</div>
 
             {/* Toggle Menu Button */}
@@ -25,29 +25,29 @@ function NavBar() {
             </div>
             <div className={`w-full lg:flex lg:items-center lg:w-auto ${isOpen ? 'block' : 'hidden'}`}>
             <ul className='flex flex-row  md:flex space-x-6'>
-                <li><a href="/" className='text-white px-4 py-2 border border-transparent rounded-md hover:bg-green-300'>Home</a></li>
-                <li><a href="/about" className='text-white px-4 py-2 border border-transparent rounded-md hover:bg-green-300'>About</a></li>
-                <li><a href="/products" className='text-white px-4 py-2 border border-transparent rounded-md hover:bg-green-300'>Productlist</a></li><br />
+                <li><a href="/" className='py-2 px-4 bg-transparent text-white-600 font-semibold border border-stone-600 rounded hover:bg-gray-300 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0'>Home</a></li>
+                <li><a href="/about" className='py-2 px-4 bg-transparent text-white-600 font-semibold border border-stone-600 rounded hover:bg-gray-300 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0'>About</a></li>
+                <li><a href="/products" className='py-2 px-4 bg-transparent text-white-600 font-semibold border border-stone-600 rounded hover:bg-gray-300 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0'>Productlist</a></li><br />
                
             </ul>
             <div>
         
-          <Link to="/login" className="text-white px-4 py-2 border border-transparent rounded-md hover:bg-green-300">
+          <Link to="/login" className="ml-3 py-2 px-4 bg-transparent text-white-600 font-semibold border border-stone-600 rounded hover:bg-gray-300 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0">
             Login
           </Link>
-          <Link to="/register" className="ml-4 text-white px-4 py-2 border border-transparent rounded-md hover:bg-green-300">
+          <Link to="/register" className="ml-4 py-2 px-4 bg-transparent text-white-600 font-semibold border border-stone-600 rounded hover:bg-gray-300 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0">
             Register
           </Link>
-          <Link to="/contact" className="ml-4 text-white px-4 py-2 border border-transparent rounded-md hover:bg-green-300">
+          <Link to="/contact" className="ml-4 py-2 px-4 bg-transparent text-white-600 font-semibold border border-stone-600 rounded hover:bg-gray-300 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0">
             Contact
           </Link>
           
 
         </div>
         <div className="pt-2 relative mx-auto text-gray-600">
-        <input className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-          type="search" name="search" placeholder="Search"/>
-        <button type="submit" className="absolute right-0 top-0 mt-5 mr-4 bg-stone-200 rounded-sm ">search</button>
+        <input className="ml-3 border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-sm text-sm focus:outline-none"
+          type="search" name="search" placeholder="type here"/>
+        <button type="submit" className="ml-2 py-2 px-4 bg-transparent text-white-600 font-semibold border border-stone-600 rounded hover:bg-gray-300 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0">search</button>
       </div>
        </div>
        </div>
