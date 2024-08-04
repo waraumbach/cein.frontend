633d27307a4fbe9a0a0b932f43cf194168c7bb43
waraumbach <waraumbach@gmail.com>
2024-08-04 13:47:56 +0700

    add fetch product list

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,14 +1,31 @@
+import { useEffect, useState } from 'react'
 import HeroSection from '../Hero/HeroSection'
 import ProductList from '../ProductList/Products'
 
-const Home =() =>{
+const Home = () => {
+    const [products, setProducts] = useState(null)
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await fetch(`http://localhost:3000/products`);
+                const data = await response.json();
+                setProducts(data);
+            } catch (error) {
+                console.error('Error fetching suggestions:', error);
+            }
+        };
+
+        fetchProducts()
+        return () => {
+        }
+    }, [])
+
 
     return (
         <>
             <HeroSection />
-            <ProductList />
-
-
+            {products !== null ? <ProductList products={products} /> : <></>}
         </>
 
     )
--- src/Components/ProductList/Products.jsx
+++ src/Components/ProductList/Products.jsx
@@ -1,21 +1,11 @@
-
-
-const products = [
-  { id: 1, name: 'Product 1', price: '$29.99', image: './src/assets/photos/products/product.jpg' },
-  { id: 2, name: 'Product 2', price: '$39.99', image: './src/assets/photos/products/product2.jpg' },
-  { id: 3, name: 'Product 3', price: '$49.99', image: './src/assets/photos/products/product.jpg' },
-  { id: 4, name: 'Product 4', price: '$59.99', image: './src/assets/photos/products/product.jpg' },
-  // Add more products as needed
-];
-
-const Products = () => {
+const Products = ({ products }) => {
   return (
     <div className="container mx-auto px-4 py-8">
       <h2 className="text-2xl font-bold mb-6">Product List</h2>
       <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {products.map((product) => (
           <div key={product.id} className="bg-white rounded-lg shadow-md overflow-hidden">
-            <img src={product.image} alt={product.name} className="w-full h-48 object-cover" />
+            <img src={product.images[0]} alt={product.name} className="w-full h-48 object-cover" />
             <div className="p-4">
               <h3 className="text-lg font-semibold">{product.name}</h3>
               <p className="text-gray-700">{product.price}</p>
