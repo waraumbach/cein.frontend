b53beb57ada4d051463237be0636ec3a72ef0b3e
waraumbach <waraumbach@gmail.com>
2024-08-10 20:00:32 +0700

    add navigate to all products

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import HeroSection from '../Hero/HeroSection'
 import ProductList from '../ProductList/Products'
 import { fetchProducts } from '../../service/product'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     const [products, setProducts] = useState(null)
@@ -25,7 +26,12 @@ const Home = () => {
     return (
         <>
             <HeroSection />
-            {products !== null ? <ProductList products={products} /> : <></>}
+            <div className='flex mx-20 my-4'>
+                <div className='flex-1'>
+                    {products !== null ? <ProductList products={products} /> : <></>}
+                </div>
+                <Link to="/products" className='p-4 bg-neutral text-white mx-10 self-end hover:bg-neutral-content'>View all products</Link>
+            </div>
         </>
 
     )
