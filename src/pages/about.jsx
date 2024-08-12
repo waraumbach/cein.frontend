407fe66ac0ba8b3d9626ab8b82b3c580fc07499f
waraumbach <waraumbach@gmail.com>
2024-08-12 22:16:46 +0700

    update img path

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -8,7 +8,7 @@ const OurStory = () => {
       <div className="max-w-4xl mx-auto bg-white rounded-xl shadow-md overflow-hidden">
         <div className="p-8 ">
           <h1 className="text-3xl font-bold text-gray-900 mb-4" >Our Story</h1>
-          <img src="/ourstory.jpg" alt="" className="h-screen w-100 rounded-lg object-cover object-center" />
+          <img src="public/ourstory.jpg" alt="" className="h-screen w-100 rounded-lg object-cover object-center" />
           
         </div>
       </div>
