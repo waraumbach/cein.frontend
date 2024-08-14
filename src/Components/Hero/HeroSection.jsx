68bff243df7f324a2963895b898e5d540f20a3e8
waraumbach <waraumbach@gmail.com>
2024-08-14 10:33:47 +0700

    get infos redirect to search

--- src/Components/Hero/HeroSection.jsx
+++ src/Components/Hero/HeroSection.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 
 
@@ -8,7 +9,7 @@ const HeroSection = () => {
         <div className="ml-2  ">
           <h1 className="text-xl md:text-xl text-gray font-bold mb-4">Treat Your Body like Your Face</h1>
           <p className="text-xl md:text-l text-gray">Creating the hightly Effective Body Care</p><br />
-          <a href="/detailsProduct" className="text-stone px-4 py-2  border-transparent border border-stone-600 rounded hover:bg-gray-300 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0text-lg">get infos</a>
+          <Link to="/search?term=" className="text-stone px-4 py-2  border-transparent border border-stone-600 rounded hover:bg-gray-300 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0text-lg">get infos</Link>
           </div>
         </div>
       </div>
